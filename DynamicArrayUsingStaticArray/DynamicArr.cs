c3788f39e2ca8bbb459e67ec3e379a7f53496b54
user-OPTgOpdGTtumdp6hWZErvw <adarsh.pratik@mitratech.com>
2024-09-03 00:53:52 +0530

    Add project files.

---
+++ DynamicArrayUsingStaticArray/DynamicArr.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicArrayUsingStaticArray
+{
+    public class DynamicArr
+    {
+        
+        int[] arr;
+        int length;
+        int capacity;
+
+        public DynamicArr() { length = 0; capacity = 2; arr = new int[capacity];}
+
+        public void Add(int item) {
+            
+            if(length+1 > capacity)
+            {
+                capacity = 2*length;
+                int[] newArr = new int[capacity];
+                for(int i = 0; i < length; i++)
+                {
+                    newArr[i] = arr[i];
+                }
+                arr = newArr;
+            }
+            arr[length++] = item;
+        }
+
+        public void Clear()
+        {
+            arr = new int[2];
+            length = 0;
+            capacity = 2;
+        }
+
+        public bool Contains(int item)
+        {
+            return arr.Contains(item);
+        }
+
+        public int IndexOf(int item)
+        {
+            for(int i = 0;i < length;i++)
+            {
+                if (arr[i] == item) return i;
+            }
+            return -1;
+        }
+
+        public void Insert(int item)
+        {
+            if(length+1 > capacity)
+            {
+                capacity = 2*length;
+                int[] newArr = new int[capacity];
+                for (int i = 0; i < length; i++)
+                {
+                    newArr[i] = arr[i];
+                }
+                arr = newArr;
+            }
+            arr[length++] = item;
+        }
+
+        public void Delete(int index)
+        {
+            if((index < 0) || (index > length - 1))
+            {
+                throw new IndexOutOfRangeException();
+            }
+            for(int i = 0, j = 0; i < length - 1; i++, j++)
+            {
+                if (i == index)
+                {
+                    j++;
+                }
+                arr[i] = arr[j];
+            }
+            length--;
+            capacity--;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder(length);
+            sb.Append('[');
+            if(length > 0)
+            {
+                for (int i = 0; i < length - 1; i++)
+                {
+                    sb.Append(arr[i] + ", ");
+                }
+                sb.Append(arr[length - 1]);
+            }
+            sb.Append(']');
+            return sb.ToString();
+        }
+    }
+}
---
+++ DynamicArrayUsingStaticArray/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DynamicArrayUsingStaticArray;
+
+Console.WriteLine("Hello, World!");
+DynamicArr arr = new DynamicArr();
+arr.Add(1);
+arr.Add(2);
+arr.Add(3);
+arr.Add(4);
+
+Console.WriteLine(arr.ToString());
+arr.Delete(2);
+Console.WriteLine("Deleted at index 2");
+Console.WriteLine(arr.ToString());
+arr.Insert(5);
+Console.WriteLine("Inserted element 5");
+Console.WriteLine(arr.ToString());
+Console.WriteLine("Is element 5 present?");
+Console.WriteLine(arr.Contains(5)?"Yes":"No");
+Console.WriteLine("Is element 6 present?");
+Console.WriteLine(arr.Contains(6) ? "Yes" : "No");
+Console.WriteLine("Index of element 6?");
+Console.WriteLine(arr.IndexOf(6));
+Console.WriteLine("Index of element 4?");
+Console.WriteLine(arr.IndexOf(4));
+Console.WriteLine("Array Cleanup!");
+arr.Clear();
+Console.WriteLine(arr.ToString());
+
