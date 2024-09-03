331dc8ea2dcf524e45b7b2008b9a69470e0b6c28
user-OPTgOpdGTtumdp6hWZErvw <adarsh.pratik@mitratech.com>
2024-09-04 02:04:46 +0530

    Code for single linked list

--- DynamicArrayUsingStaticArray/Program.cs
+++ DynamicArrayUsingStaticArray/Program.cs
@@ -1,29 +1,70 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DynamicArrayUsingStaticArray;
+using System.ComponentModel;
 
-Console.WriteLine("Hello, World!");
-DynamicArr arr = new DynamicArr();
-arr.Add(1);
-arr.Add(2);
-arr.Add(3);
-arr.Add(4);
+Console.WriteLine("Hello, World! Select the datatype:");
+Console.WriteLine("1)Dynamic Array\n2)Single LinkedList");
+string key = Console.ReadLine();
 
-Console.WriteLine(arr.ToString());
-arr.Delete(2);
-Console.WriteLine("Deleted at index 2");
-Console.WriteLine(arr.ToString());
-arr.Insert(5);
-Console.WriteLine("Inserted element 5");
-Console.WriteLine(arr.ToString());
-Console.WriteLine("Is element 5 present?");
-Console.WriteLine(arr.Contains(5)?"Yes":"No");
-Console.WriteLine("Is element 6 present?");
-Console.WriteLine(arr.Contains(6) ? "Yes" : "No");
-Console.WriteLine("Index of element 6?");
-Console.WriteLine(arr.IndexOf(6));
-Console.WriteLine("Index of element 4?");
-Console.WriteLine(arr.IndexOf(4));
-Console.WriteLine("Array Cleanup!");
-arr.Clear();
-Console.WriteLine(arr.ToString());
+switch (key)
+{
+    case "1":
+        DynamicArr arr = new DynamicArr();
+        arr.Add(1);
+        arr.Add(2);
+        arr.Add(3);
+        arr.Add(4);
+
+        Console.WriteLine(arr.ToString());
+        arr.Delete(2);
+        Console.WriteLine("Deleted at index 2");
+        Console.WriteLine(arr.ToString());
+        arr.Insert(5);
+        Console.WriteLine("Inserted element 5");
+        Console.WriteLine(arr.ToString());
+        Console.WriteLine("Is element 5 present?");
+        Console.WriteLine(arr.Contains(5) ? "Yes" : "No");
+        Console.WriteLine("Is element 6 present?");
+        Console.WriteLine(arr.Contains(6) ? "Yes" : "No");
+        Console.WriteLine("Index of element 6?");
+        Console.WriteLine(arr.IndexOf(6));
+        Console.WriteLine("Index of element 4?");
+        Console.WriteLine(arr.IndexOf(4));
+        Console.WriteLine("Array Cleanup!");
+        arr.Clear();
+        Console.WriteLine(arr.ToString());
+        break;
+    case "2":
+        SingleLinkedList<int> list = new SingleLinkedList<int>();
+        list.AddNodeToHead(1);
+        list.AddNodeToTail(2);
+        list.AddNodeToHead(0);
+        list.AddNodeToTail(10);
+        Console.WriteLine(list.ToString());
+        Console.WriteLine("Adding 20 to tail and -10 to head");
+        list.AddNodeToTail(20);
+        list.AddNodeToHead(-10);
+        Console.WriteLine(list.ToString());
+        Console.WriteLine("Removing 20 from tail and -10 from head");
+        list.RemoveNodeFromHead();
+        list.RemoveNodeFromTail();
+        Console.WriteLine(list.ToString());
+        Console.WriteLine("Checking if 2 exist?");
+        Console.WriteLine(list.Contains(2) ? "Yes" : "No");
+        Console.WriteLine("Checking if 20 exist?");
+        Console.WriteLine(list.Contains(20) ? "Yes" : "No");
+        Console.WriteLine("Checking index of 2?");
+        Console.WriteLine(list.IndexOf(2));
+        Console.WriteLine("Checking index of 20?");
+        Console.WriteLine(list.IndexOf(20));
+        Console.WriteLine("Remove element 2.");
+        list.Remove(2);
+        Console.WriteLine(list.ToString());
+        Console.WriteLine("Add null to tail");
+        break;
+    default:
+        Console.WriteLine("Invalid selection!");
+        break;
+}
+
 
---
+++ DynamicArrayUsingStaticArray/SingleLinkedList.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicArrayUsingStaticArray
+{
+    public class SingleLinkedList<T>
+    {
+        private int size;
+        private Node<T> head;
+        private Node<T> tail;
+
+        public SingleLinkedList()
+        {
+            size = 0;
+            head = null;
+            tail = null;
+        }
+
+        public class Node<T>
+        {
+            public T data;
+            public Node<T> next;
+            public Node(T data, Node<T> next)
+            {
+                this.data = data;
+                this.next = next;
+            }
+        }
+
+        public bool IsEmpty() { return size == 0; }
+
+        public void AddNodeToHead(T data)//O(1)
+        {
+            if (IsEmpty())
+            {
+                head = tail = new Node<T>(data, null);
+                size++;
+                return;
+            }
+            Node<T> node = new Node<T>(data, head);
+            node.next = head;
+            head = node;
+
+            size++;
+        }
+
+        public void AddNodeToTail(T data)//O(1)
+        {
+            if (IsEmpty())
+            {
+                head = tail = new Node<T>(data, null);
+                size++;
+                return;
+            }
+            Node<T> node = new Node<T>(data, null);
+            tail.next = node;
+            tail = node;
+            size++;
+        }
+
+        public T RemoveNodeFromHead()//O(1)
+        {
+            T data;
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Linked List is empty!");
+            }
+            Node<T> deleteNode = head;
+            head = head.next;
+            data = deleteNode.data;
+            deleteNode.next = null;
+            size--;
+            return data;
+        }
+
+        public T RemoveNodeFromTail()//O(n)
+        {
+            Node<T> temp = head;
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Linked List Empty");
+            }
+            for(int i=0; i < size - 1 ; i++)
+            {
+                temp = temp.next;
+            }
+            Node<T> deleteNode = tail;
+            deleteNode.next = null;
+            tail = temp;
+            tail.next = null;
+            size--;
+            return deleteNode.data;
+        }
+
+        public int IndexOf(T data)//O(n)
+        {
+            Node<T> temp = head;
+            int i = 0;
+            while (temp.next != null) {
+                if (temp.data == null ? data == null : temp.data.Equals(data))//handling scenario in case data is NULL
+                {
+                    return i;
+                }
+                temp = temp.next;
+                i++;
+            }
+            return -1;
+        }
+
+        public bool Contains(T data)
+        {
+            return IndexOf(data) != -1;
+        }
+
+        public bool Remove(T data) 
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Linked List Empty");
+            }
+
+            Node<T> temp = head;
+            Node<T> prev = head;
+            while(temp != null)
+            {
+                if(temp.data == null ? data == null : temp.data.Equals(data))
+                {
+                    Node<T> deleteNode = temp;
+                    prev.next = deleteNode.next;
+                    deleteNode.next = null;
+                    return true;
+                }
+                prev = temp;
+                temp = temp.next;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append('[');
+            for(Node<T> temp = head; temp.next != null; temp = temp.next)
+            { 
+                sb.Append(temp.data+", ");
+            }
+            if (!IsEmpty())
+            {
+                sb.Append(tail.data);
+            }
+            sb.Append(']');
+            return sb.ToString();
+        }
+    }
+}
